c022522d7adae5da18c42f75093bcf59bc3b7903
ponaalagar <147903591+ponaalagar@users.noreply.github.com>
2024-08-20 13:46:51 +0530

    java programing

--- Rectangle.java
+++ Rectangle.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 public class Rectangle {
-    public static int add(int a , int b)
-    {
-        return a+b;
+   public static void main(String[] args)
+   {
+       Scanner sc = new Scanner(System.in);
+       area a=new area();
+       System.out.print("enter the value of x:");
+       a.length= sc.nextInt();
+       System.out.print("enter the value of  y:");
+       a.breadth= sc.nextInt();
+       double y=a.product();
+       System.out.println(y);
     }
     
+    
+}
+class area{
+    int length;
+    int breadth;
+    double product()
+    {
+        return length*breadth;
+    }
 }
